236db5e195f41d2b8724997164be01a771259e55
Nicholas Vega <122232969+nickmvega@users.noreply.github.com>
2024-10-28 20:28:43 +0000

    hw

--- app.rb
+++ app.rb
@@ -2,8 +2,50 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:elephant)
 end
+
+get("/rock") do
+  possible_moves = ["rock", "paper", "scissors"]
+  @computer_move = possible_moves.sample
+  
+  if @computer_move == "rock"
+    @result = "We tied!"
+  elsif @computer_move == "scissors"
+    @result = "We win!"
+  else
+    @result = "We lose!"
+  end
+
+  erb(:rock)
+end
+
+get("/paper") do
+  possible_moves = ["rock", "paper", "scissors"]
+  @computer_move = possible_moves.sample
+
+  if @computer_move == "paper"
+    @result = "We tied!"
+  elsif @computer_move == "rock"
+    @result = "We win!"
+  else
+    @result = "We lose!"
+  end
+
+  erb(:paper)
+end
+
+get("/scissors") do
+  possible_moves = ["rock", "paper", "scissors"]
+  @computer_move = possible_moves.sample
+
+  if @computer_move == "scissors"
+    @result = "We tied!"
+  elsif @computer_move == "paper"
+    @result = "We win!"
+  else
+    @result = "We lose!"
+  end
+  
+  erb(:scissors)
+end 
